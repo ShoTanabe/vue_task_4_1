652893cbe65e74deae9a9e03e7e1c35af8411d63
ShoTanabe <tanabe.sho26@gmail.com>
2021-05-23 17:48:41 +0900

    vue課題④-3 1回目

--- src/store.js
+++ src/store.js
@@ -5,14 +5,19 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    idToken: null
+    idToken: null,
+    userName: null,
   },
   getters: {
-    idToken: state => state.idToken
+    idToken: state => state.idToken,
+    userName: state => state.userName
   },
   mutations: {
     updateIdToken(state, idToken) {
       state.idToken = idToken;
+    },
+    updateUserName(state, userName) {
+      state.userName = userName;
     }
   }
 });
